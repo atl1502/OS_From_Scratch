6b7a9193bd669835415235ce1a346f439234ed3d
Wu <shuenwu2@illinois.edu>
2022-10-21 15:44:46 -0500

    rtc compiles

--- student-distrib/drivers/rtc.c
+++ student-distrib/drivers/rtc.c
@@ -9,6 +9,8 @@
 
 static spinlock_t rtc_lock = SPIN_LOCK_UNLOCKED;
 
+volatile int flag;
+
 /*
  * rtc_init
  * DESCRIPTION: Initialize the RTC
@@ -34,6 +36,10 @@ void rtc_init(void) {
     outb(inb(PORT1) & RNMI, PORT1);
     //garbage throw out to prevent RTC from going into undefined state
     inb(PORT2);
+    //set rate/freq to be default
+    frequency = FREQ_ST;
+    rate = RT_ST;
+    flag = 0;
     // reenable interrupts
     spin_unlock_irq(&rtc_lock);
 }
@@ -52,5 +58,118 @@ void rtc_handle_interrupt(void) {
     inb(PORT2);
     // test_interrupts();
     //send EOI to PIC
+    flag = 0;
     send_eoi(RTC_IRQ);
+}
+
+/*
+ * rtc_open
+ * DESCRIPTION: provides access to the file system
+ * INPUTS: none
+ * SIDE EFFECTS: initializes RTC frequency to 2Hz
+ * RETURN VALUE: 0
+ */
+int rtc_open(void) {
+    frequency = FREQ_OP;
+    rate = RT_OP;
+    //disable interrupts
+    spin_lock_irq(&rtc_lock);
+    // select register A, and disable NMI
+    outb(REGA, PORT1);
+    // read the current value of register B
+    char prev=inb(PORT2);
+    // set the index again (a read will reset the index to register D)
+    outb(REGA, PORT1);
+    // write only our rate to A. Note, rate is the bottom 4 bits.
+    outb((prev & 0xF0) | rate, PORT2);
+    //reinable NMI
+    outb(inb(PORT1) & RNMI, PORT1);
+    //garbage throw out to prevent RTC from going into undefined state
+    inb(PORT2);
+    // reenable interrupts
+    spin_unlock_irq(&rtc_lock);
+    return 0;
+}
+
+/*
+ * rtc_close
+ * DESCRIPTION: closes the specified file descriptor and makes it available for return from later calls to open
+ * INPUTS: none
+ * SIDE EFFECTS: none
+ * RETURN VALUE: 0
+ */
+int rtc_close(void) {
+    return 0;
+}
+
+/*
+ * rtc_read
+ * DESCRIPTION:  reads data from RTC
+ * INPUTS: none
+ * SIDE EFFECTS: does not return until interrupt handler handled
+ * RETURN VALUE: 0
+ */
+int rtc_read(void) {
+    flag = 1;
+    while (flag != 0){
+        continue;
+    }
+    return 0;
+}
+
+/*
+ * rtc_write
+ * DESCRIPTION:  writes data to RTC
+ * INPUTS: none
+ * SIDE EFFECTS: changes RTC frequency
+ * RETURN VALUE: 0
+ */
+int rtc_write(int* buf) {
+    //get frequency
+    frequency = *buf;
+    //check if frequency is multiple of 2
+    if ((frequency == 0) || ((frequency & (frequency - 1)) != 0)) {
+        return -1;
+    }
+    //limit rate from 2 to 1024 Hz
+    if ((frequency < 2) || (frequency > 1024)) {
+        return -1;
+    }
+    rate = ret_rate(frequency);
+    //disable interrupts
+    spin_lock_irq(&rtc_lock);
+    // select register A, and disable NMI
+    outb(REGA, PORT1);
+    // read the current value of register B
+    char prev=inb(PORT2);
+    // set the index again (a read will reset the index to register D)
+    outb(REGA, PORT1);
+    // write only our rate to A. Note, rate is the bottom 4 bits.
+    outb((prev & 0xF0) | rate, PORT2);
+    //reinable NMI
+    outb(inb(PORT1) & RNMI, PORT1);
+    //garbage throw out to prevent RTC from going into undefined state
+    inb(PORT2);
+    // reenable interrupts
+    spin_unlock_irq(&rtc_lock);
+    return 0;
+}
+
+/*
+ * ret_rate
+ * DESCRIPTION:  converts frequency to rate for rtc
+ * INPUTS: none
+ * SIDE EFFECTS: none
+ * RETURN VALUE: rate integer
+ */
+int ret_rate(int freq) {
+    if (freq <= 1) {
+        return RT_ST;
+    }
+    int log = 0;
+    while (freq > 1) {
+        freq >>= 1;
+        log++;
+    }
+    return RT_OP + 1 - log;
 }
--- student-distrib/drivers/rtc.h
+++ student-distrib/drivers/rtc.h
@@ -15,6 +15,14 @@
 #define RNMI 0x7F
 #define RTC_IRQ 8
 
+#define FREQ_ST 1024
+#define RT_ST 6
+#define FREQ_OP 2
+#define RT_OP 15
+
+int rate;
+int frequency;
+
 /* Externally-visible functions */
 
 /* Initialize the RTC */
@@ -22,5 +30,15 @@ void rtc_init(void);
 
 /* Handle the RTC interrupt */
 void rtc_handle_interrupt(void);
+
+int rtc_open(void);
+
+int rtc_close(void);
+
+int rtc_read(void);
+
+int rtc_write(int*);
+
+int ret_rate(int);
 
 #endif // RTC_H
