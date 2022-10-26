6f1a4febcf01fcb6ef61fc8ecfa79518bb2b08bd
Oscar <oscarw2@illinois.edu>
2022-10-25 22:16:47 -0500

    i guess office hour will help

---
+++ student-distrib/drivers/pcb.h
@@ -0,0 +1,18 @@
+#ifndef PCB
+#define PCB
+
+#include "filesystem.h"
+#include "../fd.h"
+#include "../types.h"
+
+typedef struct pcb {
+    int pid;
+    int parent_id;
+    fd_t fd;
+    uint32_t esp;
+    uint32_t eip;
+    
+    int active; // 1 if active/started
+} pcb_t;
+
+#endif
