e9dc61bfa628b64513d1a769f383d7ab2781dce1
Wan, Oscar Dale <23065185+OscarDaleWan@users.noreply.github.com>
2022-10-27 21:47:54 -0500

    pcb moved out of drivers folder

---
+++ student-distrib/pcb.c
@@ -0,0 +1,19 @@
+#include "pcb.h"
+#include "lib.h"
+#include "types.h"
+
+// PCB is located at the TOP of current kernel stack
+// stack grows upside down
+// PCB is located at top of 8kb block
+
+pcb_t* get_pcb() {
+    // gets current stack pointer
+    void* sp = NULL;
+
+    // get curr PCB using bitmask with trick from top of file
+    // 2^13 = 8KB
+    // 0xE000 clear least significant 13 bits
+    uint32_t pcb = ( ((uint32_t) (&sp)) & 0xE000 );
+
+    // how do i get what i want from this
+}
